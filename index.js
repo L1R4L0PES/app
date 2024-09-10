5ccb898c23a0962a00113bb5dc43427c60e5dfdd
Alice Lira <alicelopeslira1999@gmail.com>
2024-09-10 11:53:56 -0300

    fundamentos 002

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
-// Hello world
-let mensagem = "hello world!"
-console.log(mensagem);
-console.log(mensagem);
-console.log(mensagem);
-console.log(mensagem);
+//  arrays objetos
+let meta = {
+    value: 'ler um livro todo mês',
+    checked: true,
+}
+
+let metas = [
+    meta,
+    {
+        value:"caminhar 20 minutos todos os dias",
+        checked: false
+    }
+]
+
+console.log(metas[1].value)
